8c34f5d1681320ae4e7360fd5e54d41fbdad3c61
Gitta P <90756673+gitta6@users.noreply.github.com>
2023-05-15 18:38:49 +0200

    .

---
+++ program/origamiOstrich/include/ostrich.h
@@ -0,0 +1,40 @@
+#ifndef OSTRICH_H
+#define OSTRICH_H
+
+#include <obj/model.h>
+#include <obj/load.h>
+#include <obj/draw.h>
+#include "texture.h"
+#include "utils.h"
+#include "camera.h"
+#include <stdbool.h>
+
+
+typedef struct Ostrich
+{
+    GLuint texture_id;
+    Model model;
+    vec3 pos;
+    vec3 speed;
+    bool forward;
+    bool backward;
+    bool left;
+    bool right;
+} Ostrich;
+
+/**
+ * Initialize the ostrich.
+*/
+void init_ostrich(Ostrich* ostrich);
+
+void set_ostrich_pos(Ostrich* ostrich, vec3 newPos);
+
+void set_ostrich_speed(Ostrich* ostrich, double speed);
+
+void set_ostrich_side_speed(Ostrich* ostrich, double speed);
+
+void set_ostrich_vertical_speed(Ostrich* ostrich, double speed);
+
+void move_ostrich(Ostrich* ostrich, double time);
+
+#endif /*OSTRICH_H*/
---
+++ program/origamiOstrich/include/scene.h
@@ -0,0 +1,66 @@
+#ifndef SCENE_H
+#define SCENE_H
+
+#include "camera.h"
+#include <obj/model.h>
+#include "GL/gl.h"
+#include "ostrich.h"
+#include "texture.h"
+#include <stdbool.h>
+#include <obj/load.h>
+#include <obj/draw.h>
+#include <stdio.h>
+
+
+typedef struct Scene
+{
+    Material material;
+    Ostrich ostrich;
+    //Material ostrichMaterial;
+    //GLuint texture_id;
+    float lightingLevel;
+    //Model desert;
+    GLuint desert_texture_id;
+    bool showInstructions;
+    GLuint instructions_texture_id;     //HELP
+    bool spectateMode;
+} Scene;
+
+/**
+ * Initialize the scene by loading models.
+ */
+void init_scene(Scene* scene);
+
+/**
+ * Set lighting.
+ */
+void set_lighting(float lightingLevel);
+
+/**
+ * Set the current material.
+ */
+void set_material(const Material* material);
+
+/**
+ * Update the scene.
+ */
+void update_scene(Scene* scene);
+
+/**
+ * Render the scene objects.
+ */
+void render_scene(const Scene* scene);
+
+/*
+* Draw the objects.
+*/
+void draw_desert(const Scene* scene);
+
+void draw_ostrich(const Scene* scene);
+
+/*
+* Show instructions.
+*/
+void instructions(GLuint texture);           //HELP
+
+#endif /* SCENE_H */
